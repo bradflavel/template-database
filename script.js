644de621460bb7617290fdcb1f493a48eec4b0f9
bradflavel <98391226+bradflavel@users.noreply.github.com>
2024-03-20 00:28:29 +1000

    Fixed up updateResultsTable function to ensure it works with latest data

--- script.js
+++ script.js
@@ -91,9 +91,9 @@ function debounce(func, wait) {
 }
 
 function updateResultsTable(filter) {
-  
     currentFilteredData = data.filter(row => 
-        row[0].toLowerCase().includes(filter) || row[2].toLowerCase().includes(filter)
+        (row[0] && row[0].toLowerCase().includes(filter)) ||
+        (row[2] && row[2].toLowerCase().includes(filter))
     );
     currentPage = 1;
     displayFilteredRows(currentFilteredData, currentPage);
