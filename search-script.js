87077ad106772e4d43acb8b81827cb8b19b8bab3
bradflavel <98391226+bradflavel@users.noreply.github.com>
2024-03-31 14:39:23 +1000

    Fixed up updateResults function

--- search-script.js
+++ search-script.js
@@ -94,6 +94,8 @@ function updateResultsTable(filter) {
   
     currentFilteredData = data.filter(row => 
         row[0].toLowerCase().includes(filter) || row[2].toLowerCase().includes(filter)
+        (row[0] && row[0].toLowerCase().includes(filter)) ||
+        (row[2] && row[2].toLowerCase().includes(filter))
     );
     currentPage = 1;
     displayFilteredRows(currentFilteredData, currentPage);
